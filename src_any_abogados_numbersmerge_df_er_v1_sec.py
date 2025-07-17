85eb0cec636b397b6641333c9a014650d9896b50
Mac1 <mac1@MacBook-Air-de-Mac1.local>
2025-07-17 14:55:11 -0500

    Actualizar numbers_merge con versión más reciente desde master

--- src_any_abogados_numbersmerge_df_er_v1_sec.py
+++ src_any_abogados_numbersmerge_df_er_v1_sec.py
@@ -7,21 +7,36 @@ from rapidfuzz.fuzz import ratio
 def normalized_phone(phone):
     if not isinstance(phone, str):
         return None
-    phone = re.split(r'\bext\b\.?', phone, flags=re.IGNORECASE)[0]
-    phone = re.sub(r'\D', '', phone)
-    return phone
+    phone = re.split(r'\bext\b\.?', phone, flags=re.IGNORECASE)[0]  # Eliminar extensión
+    phone = re.sub(r'\D', '', phone)  # Eliminar todo lo que no sea dígito
+    return phone[-7:] if len(phone) >= 7 else phone if phone else None
 
 # Función que toma dos dataframes df1 y df2 para realizar un inner merge mediante las llaves df1_colname y df2_colname respectivamente
 # Adicionalmente, se calcula el score de los nombres de los contactos según las coincidencias exactas de los números de contacto df1_name y df2_name.
 # Retorna un dataframe de coincidencias exactas por número y agrega un columna de score para comparar la calidad de la similitud de nombres
 def merge_by_contact_number(df1, df2, df1_colname, df2_colname, df1_name, df2_name):
+    
+    df1 = df1.copy()
+    df2 = df2.copy()
 
-    df_ca_o_unique = df1.copy()
-    df_naics_o_unique = df2.copy()
+    df1_colname = df1_colname.strip()
+    df2_colname = df2_colname.strip()
 
-    df_merged = pd.merge(df_ca_o_unique, df_naics_o_unique, left_on=df1_colname, right_on=df2_colname, how="inner")   
+    df1_name = df1_name.strip()
+    df2_name = df2_name.strip()
 
-    df_merged["score"] = df_merged.apply(lambda row: ratio(str(row[df1_name]), str(row[df2_name]))/100, axis=1)
+    df1[df1_colname] = df1[df1_colname].apply(normalized_phone)
+    df2[df2_colname] = df2[df2_colname].apply(normalized_phone)
+
+    df1 = df1[df1[df1_colname].notnull()]
+    df2 = df2[df2[df2_colname].notnull()]
+
+    df_merged = pd.merge(df1, df2, left_on=df1_colname, right_on=df2_colname, how="inner")
+
+    df_merged["score"] = df_merged.apply(
+        lambda row: 1 if str(row[df1_name]) == str(row[df2_name]) else 0,
+        axis=1
+    )
 
     return df_merged
 
@@ -29,7 +44,7 @@ def merge_by_contact_number(df1, df2, df1_colname, df2_colname, df1_name, df2_na
 # Se recibe el dataframe orignal df y una lista de los nombres de las columnas a combinar, en orden de prioridad.
 # Se recibe el nombre de la nueva columna que agrupa las columnas de la lista.
 # Retorna un df con la nueva columna que agrupa las demas.
-def combinar_columnas_prioridad(df, columnas, nueva_columna):
+def combine_columns_by_priority(df, columnas, nueva_columna):
 
     df[nueva_columna] = None
     for col in columnas:
